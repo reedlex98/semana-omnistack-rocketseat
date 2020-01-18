47169585ad241e00afbb40231584c776c0a0c960
Elder Louzada Alves <_rdx98@localhost.localdomain>
2020-01-18 01:37:06 -0300

    update dia 4

---
+++ mobile/src/pages/Profile.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {View} from 'react-native'
+import {WebView} from 'react-native-webview'
+
+export default function Profile({navigation}) {
+    const githubUsername = navigation.getParam('github_username')
+    return <WebView source={{uri: `https://github.com/${githubUsername}`}} />
+}
